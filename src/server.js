9a0439e678e2d4e40c8a5a05befe95db9797f69a
Your name here <kr.shampa@yahoo.com>
2020-08-11 11:18:29 -0500

    ready to deploy

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const knex = require('knex')
 const app = require('./app')
-const { PORT, DB_URL } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
 const noteService = require('./note-service')
 
 
 /* const PORT = process.env.PORT || 8000 */
 const db = knex({
  client: 'pg',
-      connection: DB_URL,
+  connection: DATABASE_URL,
     })
 
 app.set('db', db)
